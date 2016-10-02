afe2ae1961be9b06b4073ef492d3cebbea5d5f75
Frank Udnæs <frankudnes@gmail.com>
2016-10-02 14:12:21 +0200

    New Temp CD-Panels

--- web2net/lib/js/web2net-functionlib.js
+++ web2net/lib/js/web2net-functionlib.js
@@ -1,4 +1,17 @@
 
+$(document).ready(function()
+{
+    $("a.nav-link").bind("click", function(event)
+    {
+        var $anchor = $(this);
+        $("html, body").stop().animate(
+        {
+            scrollTop: $($anchor.attr("href")).offset().top
+        }, 1500, "easeInOutExpo");
+        event.preventDefault();
+    });
+});
+
 function adjustStyle()
 {
     setCSSInlineStyle();
