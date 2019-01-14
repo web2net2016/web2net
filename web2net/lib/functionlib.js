2de6d22a6a4ff2acc3ce289af66f88624033eeb9
Frank Udnæs <frankudnes601@msn.com>
2019-01-14 16:16:24 +0100

    asfasfasf

--- web2net/lib/functionlib.js
+++ web2net/lib/functionlib.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(document).ready(function ()
+{
+    $('.az-email').click(function (e)
+    {
+        var _Href = $(this).data("content").toLowerCase().replace('(at)', '@');
+        window.location.href = "mailto:" + _Href;
+        e.preventDefault();
+    });
+});
