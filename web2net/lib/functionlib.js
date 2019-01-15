0e2a431b207865fe497b9dc443628a6c7eebcc09
Frank Udnæs <frankudnes601@msn.com>
2019-01-15 10:12:08 +0100

    ny

--- web2net/lib/functionlib.js
+++ web2net/lib/functionlib.js
@@ -1,9 +1,61 @@
-﻿$(document).ready(function ()
+﻿
+// Site info
+var AppName = "web2net";
+var AppVersion = "1.0.0";
+var ApiVersion = "_1";
+
+// All Pages
+function openContactForm()
 {
-    $('.az-email').click(function (e)
+    initializeModalDialog(
     {
-        var _Href = $(this).data("content").toLowerCase().replace('(at)', '@');
-        window.location.href = "mailto:" + _Href;
-        e.preventDefault();
+        dialogNoParentScroll: true,
+        dialogDraggable: false,
+        dialogTitle: "Kontakt oss",
+        dialogWidth: 600,
+        dialogHeight: 450,
+        dialogiFrameURL: "kontakt.html"
     });
-});
+}
+
+// produkter.html
+function openStaticInfo()
+{
+    initializeModalDialog(
+    {
+        dialogModal: false,
+        dialogNoParentScroll: true,
+        dialogDraggable: false,
+        dialogTitle: "Sider og seksjoner",
+        dialogWidth: 800,
+        dialogHeight: 650,
+        dialogText: $("#StaticInfo").html()
+    });
+}
+
+function openDesign()
+{
+    $('html, body').stop().animate(
+    {
+        scrollTop: $("#accordion-2").offset().top
+    },
+    {
+        easing: 'easeInOutExpo',
+        duration: 1500
+    });
+    setAccordion(2);
+}
+
+function openSocialMedia(SelectedId)
+{
+    initializeModalDialog(
+    {
+        dialogModal: false,
+        dialogNoParentScroll: true,
+        dialogDraggable: false,
+        dialogTitle: SelectedId,
+        dialogWidth: 450,
+        dialogHeight: 200,
+        dialogText: $("#" + SelectedId).html()
+    });
+}
